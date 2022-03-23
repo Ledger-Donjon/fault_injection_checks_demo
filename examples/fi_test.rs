c53ff4244de70e998d9951f3f4fa43c9919fe2d2
Alexandre Iooss <alexandre.iooss@ledger.fr>
2022-03-23 14:28:32 +0100

    Replace if equal panic by assert_eq

--- examples/fi_test.rs
+++ examples/fi_test.rs
@@ -14,54 +14,35 @@ const REF_PIN: [u8; 4] = [1, 2, 3, 4];
 
 #[test_fi]
 pub fn simple() {
-    if compare_pin(&[0; 4], &REF_PIN) == false {
-        nominal_behavior()
-    } else {
-        panic!("faulted")
-    }
+    assert_eq!(compare_pin(&[0; 4], &REF_PIN), false);
+    nominal_behavior()
 }
 
 #[test_fi]
-pub fn double_call() {
+pub fn double() {
     let user_pin = [0; 4];
-    if compare_pin(&user_pin, &REF_PIN) == false {
-        nominal_behavior()
-    } else {
-        if compare_pin(&REF_PIN, &user_pin) == false {
-            nominal_behavior()
-        } else {
-            panic!("faulted")
-        }
-    }
+    assert_eq!(compare_pin_double(&user_pin, &REF_PIN), false);
+    nominal_behavior()
 }
 
 #[test_fi]
 pub fn simple_fp() {
-    if compare_pin_fp(&[0; 4], &REF_PIN) == false {
-        nominal_behavior()
-    } else {
-        panic!("faulted")
-    }
+    assert_eq!(compare_pin_fp(&[0; 4], &REF_PIN), false);
+    nominal_behavior()
 }
 
 #[test_fi]
 pub fn simple_fp2() {
-    if compare_pin_fp(&[1, 0, 0, 0], &REF_PIN) == false {
-        nominal_behavior()
-    } else {
-        panic!("faulted")
-    }
+    assert_eq!(compare_pin_fp(&[1, 0, 0, 0], &REF_PIN), false);
+    nominal_behavior()
 }
 
 const REFPIN: pin_verif::IntegrityProtected<[u8; 4]> = pin_verif::IntegrityProtected(REF_PIN);
 
 #[test_fi]
 pub fn hard() {
-    if (REFPIN == &[0; 4]) == false {
-        nominal_behavior()
-    } else {
-        panic!("faulted")
-    }
+    assert_eq!((REFPIN == &[0; 4]), false);
+    nominal_behavior()
 }
 
 #[test_fi]
@@ -69,17 +50,14 @@ pub fn hard2() {
     let ref_pin = pin_verif::IntegrityProtected([
         1, 8, 9, 2, 3, 1, 3, 2, 1, 0, 2, 23, 29381, 281, 283, 172, 381, 280,
     ]);
-    if (ref_pin == &[1; 18]) == false {
-        nominal_behavior()
-    } else {
-        panic!("faulted")
-    }
+    assert_eq!((ref_pin == &[1; 18]), false);
+    nominal_behavior()
 }
 
 #[no_mangle]
 pub fn _start() {
     simple();
-    double_call();
+    double();
     simple_fp();
     simple_fp2();
     hard();
