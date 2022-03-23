d1cd298b2e4f613080fba1d8fa84dab682468ad6
Alexandre Iooss <alexandre.iooss@ledger.fr>
2022-03-23 15:47:37 +0100

    Remove pub on fault tests

--- examples/fi_test.rs
+++ examples/fi_test.rs
@@ -7,35 +7,35 @@ use test_fi_macro::test_fi;
 const REF_PIN: [u8; 4] = [1, 2, 3, 4];
 
 #[test_fi]
-pub fn simple() {
+fn simple() {
     assert_eq!(compare_pin(&[0; 4], &REF_PIN), false);
 }
 
 #[test_fi]
-pub fn double() {
+fn double() {
     let user_pin = [0; 4];
     assert_eq!(compare_pin_double(&user_pin, &REF_PIN), false);
 }
 
 #[test_fi]
-pub fn simple_fp() {
+fn simple_fp() {
     assert_eq!(compare_pin_fp(&[0; 4], &REF_PIN), false);
 }
 
 #[test_fi]
-pub fn simple_fp2() {
+fn simple_fp2() {
     assert_eq!(compare_pin_fp(&[1, 0, 0, 0], &REF_PIN), false);
 }
 
 const REFPIN: pin_verif::IntegrityProtected<[u8; 4]> = pin_verif::IntegrityProtected(REF_PIN);
 
 #[test_fi]
-pub fn hard() {
+fn hard() {
     assert_eq!((REFPIN == &[0; 4]), false);
 }
 
 #[test_fi]
-pub fn hard2() {
+fn hard2() {
     let ref_pin = pin_verif::IntegrityProtected([
         1, 8, 9, 2, 3, 1, 3, 2, 1, 0, 2, 23, 29381, 281, 283, 172, 381, 280,
     ]);
